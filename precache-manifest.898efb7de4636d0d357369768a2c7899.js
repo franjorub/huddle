9fe234423d808a0e3393442a0bbf46095467b5c7
Francisco Rubial <franjorub@gmail.com>
2019-01-27 15:29:12 -0400

    Updates

---
+++ precache-manifest.898efb7de4636d0d357369768a2c7899.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cb867305e954abaa1a8b0dd7a4e7c06",
+    "url": "/huddle/static/media/logo.3cb86730.svg"
+  },
+  {
+    "revision": "8a5af9952fb5ed23779d589621b108a5",
+    "url": "/huddle/static/media/illustration-your-users.8a5af995.svg"
+  },
+  {
+    "revision": "88090acec5276438b135a468364edf12",
+    "url": "/huddle/static/media/illustration-mockups.88090ace.svg"
+  },
+  {
+    "revision": "429adb545bb690e08811",
+    "url": "/huddle/static/js/runtime~main.429adb54.js"
+  },
+  {
+    "revision": "693729e5f6c6f21bd65f",
+    "url": "/huddle/static/js/main.693729e5.chunk.js"
+  },
+  {
+    "revision": "d950344477aae3497d2b",
+    "url": "/huddle/static/js/1.d9503444.chunk.js"
+  },
+  {
+    "revision": "693729e5f6c6f21bd65f",
+    "url": "/huddle/static/css/main.562528fc.chunk.css"
+  },
+  {
+    "revision": "8e70380e14a3f7043f5522066f93a336",
+    "url": "/huddle/index.html"
+  }
+];
